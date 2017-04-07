73533551288768f0747bda3d848f92bc3202d8fb
童哥哥 <838103910@qq.com>
2017-04-07 09:00:07 +0800

    ws url

--- src/main/java/com/red/constant/CommonConstants.java
+++ src/main/java/com/red/constant/CommonConstants.java
@@ -7,6 +7,6 @@ public class CommonConstants {
 
     public static final String ROOM_USER_FORMAT = "%s_%s";
     public static final String URL = "https://sapi.kktv1.com/meShow/entrance?parameter=%s";
-    public static final String WS_URL = "http://into1.kktv8.com?roomId=%s";
+    public static final String WS_URL = "http://into1.kktv8.com/?roomId=%s";
 
 }
