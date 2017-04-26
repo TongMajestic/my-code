df65b820f9ef869821446c34b3160575080fa3ec
童哥哥 <838103910@qq.com>
2017-04-26 10:10:39 +0800

    add time sout

--- src/main/java/com/red/thread/UserInfoThread.java
+++ src/main/java/com/red/thread/UserInfoThread.java
@@ -3,6 +3,8 @@ package com.red.thread;
 import com.red.util.DataUtil;
 import com.red.util.OperUtil;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -11,6 +13,8 @@ import java.util.concurrent.TimeUnit;
 public class UserInfoThread {
 
     ScheduledExecutorService schedule = Executors.newSingleThreadScheduledExecutor();
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public void execute() {
         //延迟10秒执行，每60分钟执行一次
@@ -21,7 +25,7 @@ public class UserInfoThread {
                     String userId = DataUtil.USER_TOKEN.get(0);
                     String token = DataUtil.USER_TOKEN.get(1);
                     int amount = OperUtil.getUserInfo(userId, token);
-                    System.out.println(userId + " has money: " + amount / 1000);
+                    System.out.println(sdf.format(new Date()) + "  " + userId + " has money: " + amount / 1000);
                 } catch (Exception e) {
                     System.out.println(" UserResidualExecutor error !");
                     e.printStackTrace();
@@ -30,6 +34,5 @@ public class UserInfoThread {
             }
         }, 10, 60 * 60, TimeUnit.SECONDS);
     }
-
 
 }
